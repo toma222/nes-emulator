a69d9f09f2fece63d91f81f684bbef98c0587323
Aidan Wilhite <aidannwilhite@gmail.com>
2024-09-27 13:54:21 -0500

    drawing out the cpu

---
+++ src/cpu.rs
@@ -0,0 +1,49 @@
+
+// as defined in http://www.6502.org/users/obelisk/6502/registers.html
+
+struct CPU
+{
+  /// Points to the next program to be executed
+  pub program_counter: u16,
+
+  /// Points to a 256 byte stack located between 0x0100 and 0x01FF
+  pub stack_pointer: u8,
+
+  /// Used for arethmetic operations
+  pub accumulator: u8,
+
+  /// holds counters and offsets for accsessing memory
+  pub index_register_x: u8,
+
+  /// same as x
+  pub index_register_y: u8,
+
+  /// Holds flags for when opeations are done
+  pub processor_status: u8,
+}
+
+impl CPU
+{
+  fn new() -> CPU
+  {
+    return CPU{
+      program_counter: 0,
+      stack_pointer: 0,
+      accumulator: 0,
+      index_register_x: 0,
+      index_register_y: 0,
+      processor_status: 0
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cpu_new() {
+        let cpu = CPU::new();
+        assert_eq!(cpu.index_register_x, 0);
+    }
+}
